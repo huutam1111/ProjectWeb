ffc0d3383084de60a5aab0e4d5c30c0e32df4952
phuochau132 <20130254@st.hcmuaf.edu.vn>
2022-11-10 18:25:12 +0700

    Hoan thanh Page đăng tin

--- src/main/webapp/eventjs/HeaderActive.js
+++ src/main/webapp/eventjs/HeaderActive.js
@@ -1,16 +1,15 @@
 var locationpath = location.pathname;
 var ul = document.querySelectorAll(".menu-top>li")
 for (let i = 0; i < ul.length; i++) {
-    console.log(locationpath == "/Page/PriceCar.jsp")
+    console.log(locationpath)
     if (locationpath == "/Page/Filter.jsp") {
         ul[0].classList.add("active", "activemenu")
     } else {
-        if (locationpath == "/Page/PriceCar.jsp") {
-            console.log(123)
-            ul[2].classList.add("active", "activemenu")
-        } else {
-
-        }
+        // if (locationpath == "/asaasd/Page/Filter.jsp") {
+        //     ul[1].classList.add("active", "activemenu")
+        // } else {
+        //
+        // }
     }
 }
 
